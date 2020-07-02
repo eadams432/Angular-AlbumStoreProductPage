f8c96d08d62d0a12e2d4b7c998c7eeb414f968cc
ethan <era432@gmail.com>
2020-07-01 21:26:06 -0500

    finalize product description

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -11,7 +11,9 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   public getAlbum(id : number)  {
-    return this._http.get(this._albumUrl);
+    return this._http.get(this._albumUrl).map((response,index)=>{
+      response.json();
+    })
   }
 
 }
